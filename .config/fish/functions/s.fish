3c63c2624cf3aaf21b982f9439af9d23f7a95d8e
crazybanboo <704996718@qq.com>
2020-11-22 18:20:56 +0800

    add .x.... & .config's something useful
    Signed-off-by: crazybanboo <704996718@qq.com>

---
+++ .config/fish/functions/s.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function s --wraps=screenfetch --description 'alias s screenfetch'
+  screenfetch  $argv;
+end
---
+++ .dwm/autostart.sh
@@ -0,0 +1,2 @@
+xset -r r rate 250 &
+picom -b --vsync -i 0.88 &
